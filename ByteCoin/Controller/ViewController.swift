f62c07524e1bcc39c3265d452bd773f8e00f12dc
Denis Chernov <chernoff.cs@gmail.com>
2023-01-09 18:35:50 +0200

    Files uploading

---
+++ ByteCoin/Controller/ViewController.swift
@@ -0,0 +1,56 @@
+
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var coinManager = CoinManager()
+
+    @IBOutlet weak var currencyLabel: UILabel!
+    @IBOutlet weak var bitcoinLabel: UILabel!
+    @IBOutlet weak var currencyPicker: UIPickerView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        coinManager.delegate = self
+        currencyPicker.dataSource = self
+        currencyPicker.delegate = self
+        
+    }
+}
+
+//MARK: - CoinManagerProtocol
+
+extension ViewController: CoinManagerProtocol{
+    func didUpdatePrice(price: String, currency: String) {
+        DispatchQueue.main.async {
+            self.bitcoinLabel.text = price
+            self.currencyLabel.text = currency
+        }
+    }
+    
+    func didFailWithError(error: Error) {
+        print(error)
+    }
+}
+
+//MARK: - Picker
+
+extension ViewController: UIPickerViewDataSource, UIPickerViewDelegate{
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let currentCoin = coinManager.currencyArray[row]
+        coinManager.getCoinPrice(for: currentCoin)
+    }
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return coinManager.currencyArray.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return coinManager.currencyArray[row]
+    }
+
+}
---
+++ ByteCoin/Model/CoinData.swift
@@ -0,0 +1,8 @@
+
+
+import Foundation
+
+struct CoinData: Codable{
+    let rate: Double
+}
+
